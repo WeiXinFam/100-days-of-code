3be7ae5e6a964c36a2159e80f9bc05a13ba18ff3
Wei Xin Fam <u6554719@anu.edu.au>
2020-06-02 20:29:18 +1000

    Day 2-4

---
+++ Mongo&Node/index.js
@@ -0,0 +1,7 @@
+// console.log('Hello From Node.js')
+// Run JS file in terminal by: node <filename>
+
+const Person = require('./person');
+const p1 = new Person('John',28)
+p1.greeting();
+// to get specific person attribute, put person.name
---
+++ Mongo&Node/person.js
@@ -0,0 +1,17 @@
+// const person ={
+//     name:'John',
+//     age: 28
+// }
+// module.exports=person; //to export const person variable  
+
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    greeting(){ //method
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+    }
+}
+
+module.exports=Person;  //to export Person class

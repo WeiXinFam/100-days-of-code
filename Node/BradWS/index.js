ae9a66ed2d8d1e10a90e924f669979b58ed69b69
Wei Xin Fam <u6554719@anu.edu.au>
2020-06-04 15:55:10 +1000

    Day 6

--- Node/BradWS/index.js
+++ Node/BradWS/index.js
@@ -1,3 +1,4 @@
+// Initial
 // console.log('Hello From Node.js')
 // Run JS file in terminal by: node <filename>
 
@@ -6,11 +7,113 @@
 // p1.greeting();
 // to get specific person attribute, put person.name
 
-const Logger = require('./logger');
+//Second
+// const Logger = require('./logger');
 
-const logger = new Logger();
+// const logger = new Logger();
 
-logger.on('message',(data)=>console.log('Called Listener: ', data));
-logger.log('Hello World');
+// logger.on('message',(data)=>console.log('Called Listener: ', data));
+// logger.log('Hello World');
 
-//To try tmr: using these modeules to key in and listen to what the user is writing
+//To try: using these modules to key in and listen to what the user is writing
+
+// Third
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url) //returns url on the port
+    
+    //These won't work as we are reading in every single HTML file
+    // //Load HTML
+    // if(req.url == '/'){ 
+        
+    //     fs.readFile(path.join(__dirname,'public','index.html') , (err, content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.end(content);
+    //     })
+        
+    // }
+
+    // if(req.url == '/about'){ 
+    //     //Load HTML
+    //     fs.readFile(path.join(__dirname,'public','about.html') , (err, content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.end(content);
+    //     })
+        
+    // }
+
+    // // LOAD API with JSON. Normally use database and Express.JS, in this case, use hardcode. 
+    // // Try using database next time
+    // if(req.url == '/api/users'){ 
+    //    const users =[
+    //        { name: 'Bob Smith', age: 40},
+    //        { name: 'John Doe', age:30}
+    //    ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'})
+    //     res.end(JSON.stringify(users));
+    // }
+
+    //Dynamic method
+    //Build file path
+    let filePath = path.join(__dirname,'public', req.url === '/' ? 'index.html' : req.url);
+
+    // Extension of file
+    let extName = path.extname(filePath);
+
+    //Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch(extName){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;           
+    }
+
+    //Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if (err.code == 'ENOENT'){
+                //Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf8');
+                })
+            }
+            else{
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }else{
+            //Sucess
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf8');
+        }
+    });
+
+   
+});
+
+const PORT = process.env.PORT || 5000; //Checks env variable first then only go to PORT 5000
+
+server.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
+
+
